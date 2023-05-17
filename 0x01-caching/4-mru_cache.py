59003ed25058533af769b46244a7cce06bd67bd3
Domengo <dominicsengo@gmail.com>
2023-05-17 18:52:49 +0300

    MRUCache

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """MRUCache"""
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
@@ -10,23 +11,31 @@ class MRUCache(BaseCaching):
         """Constructor
         """
         super().__init__()
-        self.lru = None
+        self.mru_order = OrderedDict()
 
     def put(self, key, item):
         """Add an item in the cache
         """
-        if key and item:
-            self.cache_data[key] = item
-            self.lru = key
+        if not key or not item:
+            return
+
+        self.cache_data[key] = item
+        self.mru_order[key] = item
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discard = self.lru
-            del self.cache_data[discard]
-            print('DISCARD: {}'.format(discard))
+            item_discarded = next(iter(self.mru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+
+        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
+            self.mru_order.popitem(last=False)
+
+        self.mru_order.move_to_end(key, False)
 
     def get(self, key):
         """Get an item by key
         """
         if key in self.cache_data:
-            self.lru = key
+            self.mru_order.move_to_end(key, False)
             return self.cache_data[key]
         return None
