c07c1c8677b37714bec894ddf96b1352431c45c7
Domengo <dominicsengo@gmail.com>
2023-05-17 18:45:43 +0300

    LIFO Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
-"""MRUCache"""
-from base_caching import BaseCaching
+"""
+LIFO Caching
+"""
 
 
-class MRUCache(BaseCaching):
-    """MRU Cache class
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
     """
+     class LIFOCache that inherits from BaseCaching and is a caching system
+    """
+
     def __init__(self):
-        """Constructor
+        """
+        Init method
         """
         super().__init__()
-        self.lru = None
+        self.key_indexes = []
 
     def put(self, key, item):
-        """Add an item in the cache
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
         """
         if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
+
             self.cache_data[key] = item
-            self.lru = key
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discard = self.lru
-            del self.cache_data[discard]
-            print('DISCARD: {}'.format(discard))
+            self.key_indexes.append(key)
 
     def get(self, key):
-        """Get an item by key
+        """
+        Must return the value in self.cache_data linked to key.
         """
         if key in self.cache_data:
-            self.lru = key
             return self.cache_data[key]
         return None
