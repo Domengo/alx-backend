a981f3ef6ce8523d14f032e3b03eae1986bb9d27
Domengo <dominicsengo@gmail.com>
2023-05-23 19:14:41 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,7 +22,8 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match for the supported languages using request.accept_languages
+    """Determine the best match for the supported
+    languages using request.accept_languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
