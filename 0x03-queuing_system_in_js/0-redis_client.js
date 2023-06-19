6f9ce7b5b3c775337a3e5edfff8a19a3fd733173
Domengo <dominicsengo@mail.com>
2023-06-19 15:58:26 +0300

    Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const client = createClient();
 
-client.on("connect", () => {
-    console.log("Redis client connected to the server");
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
 });
 
-client.on("error", (err) => {
-    console.log(`Redis client not connected to the server: {err}`);
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
 });
